8c9113a84ac377c0b73ea86cc098320af8a4534e
Ruan Engelbrecht <aj.rengelbr@gmail.com>
2023-09-30 22:17:24 +0200

    feat: blur placeholder for profile image

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import profile from '@/assets/images/profile.jpg';
 
 export default function Home() {
   return (
@@ -6,10 +7,12 @@ export default function Home() {
       <div className="flex flex-col justify-start items-center gap-12">
         <Image
           className="rounded-2xl ring-2 ring-white shadow-xl shadow-white/20"
-          src="/profile.jpg"
+          src={profile}
           width={300}
           height={300}
           alt="Profile image"
+          placeholder="blur"
+          quality={60}
         />
         <div className="space-y-6">
           <h1 className="text-6xl font-semibold">Ruan Engelbrecht</h1>
