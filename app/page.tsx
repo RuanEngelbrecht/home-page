78109e52925bd10bd1d50fb71c7238a7b6dcd124
Ruan Engelbrecht <aj.rengelbr@gmail.com>
2023-10-01 14:04:02 +0200

    feat: responsive styles

--- app/page.tsx
+++ app/page.tsx
@@ -8,17 +8,20 @@ export default function Home() {
     <main className="h-screen flex flex-row justify-center items-center">
       <div className="flex flex-col justify-start items-center gap-12">
         <Image
-          className="rounded-2xl ring-2 ring-white shadow-xl shadow-white/20"
+          className="rounded-2xl ring-2 ring-white shadow-xl shadow-white/20 h-[150px] w-[150px] md:h-[250px] md:w-[250px]"
           src={profile}
           width={250}
           height={250}
           alt="Profile image"
           placeholder="blur"
           quality={60}
+          sizes="(max-width: 640px) 150px, 250px"
         />
         <div className="space-y-6">
-          <h1 className="text-6xl font-semibold">Ruan Engelbrecht</h1>
-          <p className="text-xl font-mono text-center text-white/40">
+          <h1 className="text-4xl font-semibold md:text-6xl">
+            Ruan Engelbrecht
+          </h1>
+          <p className="text-lg font-mono text-center text-white/40 md:text-xl">
             {'<Software Engineer />'}
           </p>
         </div>
